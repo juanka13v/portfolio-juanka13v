1f9f400dcf89fa5c3ed1e6c44f381b552d4c16ca
juanka13v <juanka13v@users.noreply.github.com>
2023-11-15 23:29:12 -0300

    update cofig and add link boxicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 };
