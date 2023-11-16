e7384076f3afdb00200013ab876de8af958a243a
juanka13v <juanka13v@users.noreply.github.com>
2023-11-15 23:27:44 -0300

    feat(contacts): add DataContact

---
+++ src/constants.jsx
@@ -0,0 +1,32 @@
+export const  DataContact = [
+    {
+        icon: "bx bxs-envelope",
+        title: "Email",
+        description: "Send me a message on email.",
+        url: null,
+        data: "juanka13@gmail.com"
+    },
+    {
+        icon: "bx bxs-phone",
+        title: "Phone Number",
+        description: "Give me a call for quick assistance.",
+        url: null,
+        data: "+54 4233-1233"
+    },
+    {
+        icon: "bx bxl-linkedin",
+        title: "LinkedIn",
+        description: "Connect with me on LinkedIn.",
+        url: "https://www.linkedin.com",
+        subIcon: "bx bx-link-alt",
+        data: "@juanka13v"
+    },
+    {
+        icon: "bx bxl-twitter",
+        title: "Twitter",
+        description: "You can send me a message on Twitter",
+        url: "https://www.twitter.com",
+        subIcon: "bx bx-link-alt",
+        data: "@juanka13v"
+    }
+] 
