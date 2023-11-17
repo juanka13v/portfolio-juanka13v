37d6ac3291d1f149014140ec368a9219ec03d5ec
juanka13v <juanka13v@users.noreply.github.com>
2023-11-17 15:24:09 -0300

    refactor(constants): add DataSkills

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,32 +1,92 @@
-export const  DataContact = [
+export const DataContact = [
+  {
+    icon: "bx bxs-envelope",
+    title: "Email",
+    description: "Send me a message on email.",
+    url: null,
+    data: "juanka13@gmail.com",
+  },
+  {
+    icon: "bx bxs-phone",
+    title: "Phone Number",
+    description: "Give me a call for quick assistance.",
+    url: null,
+    data: "+54 4233-1233",
+  },
+  {
+    icon: "bx bxl-linkedin",
+    title: "LinkedIn",
+    description: "Connect with me on LinkedIn.",
+    url: "https://www.linkedin.com",
+    subIcon: "bx bx-link-alt",
+    data: "@juanka13v",
+  },
+  {
+    icon: "bx bxl-twitter",
+    title: "Twitter",
+    description: "You can send me a message on Twitter",
+    url: "https://www.twitter.com",
+    subIcon: "bx bx-link-alt",
+    data: "@juanka13v",
+  },
+];
+
+export const DataSkills = {
+  frontend: [
     {
-        icon: "bx bxs-envelope",
-        title: "Email",
-        description: "Send me a message on email.",
-        url: null,
-        data: "juanka13@gmail.com"
+      title: "html",
+      icon: "bx bxl-html5",
     },
     {
-        icon: "bx bxs-phone",
-        title: "Phone Number",
-        description: "Give me a call for quick assistance.",
-        url: null,
-        data: "+54 4233-1233"
+      title: "css",
+      icon: "bx bxl-css3",
     },
     {
-        icon: "bx bxl-linkedin",
-        title: "LinkedIn",
-        description: "Connect with me on LinkedIn.",
-        url: "https://www.linkedin.com",
-        subIcon: "bx bx-link-alt",
-        data: "@juanka13v"
+      title: "js",
+      icon: "bx bxl-javascript",
     },
     {
-        icon: "bx bxl-twitter",
-        title: "Twitter",
-        description: "You can send me a message on Twitter",
-        url: "https://www.twitter.com",
-        subIcon: "bx bx-link-alt",
-        data: "@juanka13v"
-    }
-] 
+      title: "typescript",
+      icon: "bx bxl-typescript",
+    },
+    {
+      title: "react",
+      icon: "bx bxl-react",
+    },
+    {
+      title: "redux",
+      icon: "bx bxl-redux",
+    },
+
+    {
+      title: "tailwind",
+      icon: "bx bxl-tailwind-css",
+    },
+  ],
+  backend: [
+    {
+      title: "node js",
+      icon: "bx bxl-nodejs",
+    },
+    {
+      title: "mongodb",
+      icon: "bx bxl-mongodb",
+    },
+    {
+      title: "docker",
+      icon: "bx bxl-docker",
+    },
+    {
+      title: "postgresql",
+      icon: "bx bxl-postgresql",
+    },
+    {
+      title: "python",
+      icon: "bx bxl-python",
+    },
+    {
+      title: "django",
+      icon: "bx bxl-django",
+    },
+  ],
+};
