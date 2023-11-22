b9339bbba90593cf289c5aa3a1be1318ef3558e5
juanka13v <juanka13v@users.noreply.github.com>
2023-11-22 05:14:29 -0300

    feat(App): add components to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import Hero from "@components/Hero";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import Contact from "@components/Contact";
+import Skills from "@components/Skills";
+import Services from "@components/Services";
+import Projects from "@components/Projects";
 
 function App() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <main className="bg-base-100 mx-auto max-w-screen-xl px-4 md:px-6 flex flex-col gap-8 ">
+        <Header />
+        <Hero />
+        <Skills />
+        <Projects />
+        <Services />
+        <Contact />
+        <Footer />
+      </main>
     </>
   );
 }
